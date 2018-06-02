c92e843e34d5d01f9d3fc285a5ed5370dbb6e9dc
Raghu Betina <raghu@firstdraft.com>
2018-06-02 02:16:03 +0000

    Whitelist columns in Step for ActiveAdmin

--- app/admin/steps.rb
+++ app/admin/steps.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Step do
+
+ permit_params :recipe_id, :step_number, :direction
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
